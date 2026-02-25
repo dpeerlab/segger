41e300763c5e5bd38635214a34c28126bf02898e
tobiaspk <tobiaspk1@gmail.com>
2026-02-25 12:51:56 -0500

    Implement debug predict and segment only CLI

--- src/segger/cli/debug.py
+++ src/segger/cli/debug.py
@@ -2,14 +2,17 @@
 Debugging utilities for Segger.
 """
 
+import os
 from pathlib import Path
 from typing_extensions import Annotated
 from cyclopts import App, Parameter
+
+from ..debug.segmentation import run_segmentation_only
 from ..debug.prediction import run_prediction_only
 
 debug = App(name="debug", help="Utilities for debugging and testing individual components.")
 
-@debug.command(name="predict-only")
+@debug.command(name="segment-only")
 def predict_only(
     path_adata: Annotated[Path, Parameter(
         help="Path to input AnnData object.",
@@ -22,8 +25,23 @@ def predict_only(
     )],
 ):
     """Run prediction only."""
-    run_prediction_only(
+    run_segmentation_only(
         path_adata=path_adata,
         path_predictions=path_predictions,
+        path_outputs=path_outputs,
+    )
+
+@debug.command(name="predict-only")
+def predict_only_cli(
+    path_checkpoint: Annotated[Path, Parameter(
+        help="Path to trainer checkpoint object.",
+    )],
+    path_outputs: Annotated[Path, Parameter(
+        help="Path to write outputs.",
+    )],
+):
+    """Run prediction only."""
+    run_prediction_only(
+        path_checkpoint=path_checkpoint,
         path_outputs=path_outputs,
     )
--- src/segger/debug/prediction.py
+++ src/segger/debug/prediction.py
@@ -1,33 +1,30 @@
-"""
-Run only prediction.
-"""
+"""Run only prediction, followed by segmentation."""
 
-import pickle
-import anndata as ad
+import os
 
 def run_prediction_only(
-    path_adata,
-    path_predictions,
+    path_checkpoint,
     path_outputs,
 ):
-    """Run prediction only."""
+    from segger.data import ISTDataModule
+    from segger.data import ISTSegmentationWriter
+    from segger.models import LitISTEncoder
 
-    # imports
-    from ..data.writer import ISTSegmentationWriter
+    from lightning.pytorch.loggers import CSVLogger
+    from lightning.pytorch import Trainer
+
     from lightning.pytorch.plugins.environments import SLURMEnvironment
     SLURMEnvironment.detect = lambda: False
 
-    # Load data
-    writer = ISTSegmentationWriter(path_outputs)
-    adata = ad.read_h5ad(path_adata)
-    with open(path_predictions, "rb") as f:
-        predictions = pickle.load(f)
+    os.makedirs(path_outputs, exist_ok=True)
 
-    # Predict and write output
-    segmentation = writer.assign_transcripts_to_cells(
-        obs=adata.obs,
-        predictions=predictions,
-    )
+    # load objects (analogous to segment.py)
+    csvlogger = CSVLogger(path_outputs)
+    writer = ISTSegmentationWriter(path_outputs, debug=True)
 
-    segmentation.write_parquet(path_outputs / 'segger_segmentation.parquet')
+    trainer = Trainer(logger=csvlogger, reload_dataloaders_every_n_epochs=1, callbacks=[writer],)
+    datamodule = ISTDataModule.load_from_checkpoint(path_checkpoint)
+    model = LitISTEncoder.load_from_checkpoint(path_checkpoint)
 
+    # predict (and save results via callback)
+    predictions = trainer.predict(model=model, datamodule=datamodule)
