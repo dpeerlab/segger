c9940f6233dbf40ef44bfe12f6c25ba100c9e977
tobiaspk <tobiaspk1@gmail.com>
2026-03-11 16:25:28 -0400

    Undo debug rapids code

--- src/segger/cli/main.py
+++ src/segger/cli/main.py
@@ -1,7 +1,6 @@
 from cyclopts import App
 from .segment import segment
 from .debug import debug
-from .debug_rapids import debug_rapids
 
 # CLI App
 app = App(name="Segger")
@@ -11,5 +10,3 @@ app.command(segment)
 
 # Debugging utilities
 app.command(debug)
-
-app.command(debug_rapids)
